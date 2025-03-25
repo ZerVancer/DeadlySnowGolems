caa0ee87537953090041b56016ccd97c889bc2c2
LeoGamerMan <mirzajanzadeh.leo@gmail.com>
2025-03-25 09:58:59 +0100

    first commit

---
+++ src/main/java/net/zervancer/deadlysnowgolems/DeadlySnowGolems.java
@@ -0,0 +1,14 @@
+package net.zervancer.deadlysnowgolems;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DeadlySnowGolems implements ModInitializer {
+	public static final String MOD_ID = "deadly-snow-golems";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {}
+}
---
+++ src/main/java/net/zervancer/deadlysnowgolems/mixin/MixinSnowballEntity.java
@@ -0,0 +1,39 @@
+package net.zervancer.deadlysnowgolems.mixin;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.passive.SnowGolemEntity;
+import net.minecraft.entity.projectile.thrown.SnowballEntity;
+import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.world.World;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(SnowballEntity.class)
+public class MixinSnowballEntity extends ThrownItemEntity {
+    
+	public MixinSnowballEntity(EntityType<? extends SnowballEntity> entityType, World world) {
+		super(entityType, world);
+	}
+
+	public MixinSnowballEntity(World world, LivingEntity owner) {
+		super(EntityType.SNOWBALL, owner, world);
+	}
+
+	public MixinSnowballEntity(World world, double x, double y, double z) {
+		super(EntityType.SNOWBALL, x, y, z, world);
+	}
+
+    @Override
+	public Item getDefaultItem() {
+		return Items.SNOWBALL;
+	}
+	@ModifyConstant(method = "onEntityHit(Lnet/minecraft/util/hit/EntityHitResult;)V", constant = @Constant(intValue = 0))
+    private int injected(int value) {
+        return this.getOwner() instanceof SnowGolemEntity ? 1 : 0;
+    }
+}
